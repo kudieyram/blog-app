5a120a2600feb110c84054756fc724aa372227b1
kudi_eyram <noraeyram.kudiabor@gmail.com>
2020-11-18 20:48:02 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //import logo from './logo.svg';
 import './App.css';
 import SigInComponent from './components/SignIn';
+import SignUpComponent from './components/SignUp';
 
 function App() {
   return (
-    <SigInComponent/>
+    <SignUpComponent/>
   );
 }
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,45 +1,59 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function SignUpComponent (){
     const handleclick = () =>{
          alert('😍Sign Up Successful!')
         }
     return(
-        <form>
-            <h3 ClassName='lgup'>Sign Up to Create Soronko User Account</h3>
-                <label for= "fname">First Name</label><br/>
-                <input type="text" id="fname" className="fname" placeholder='Your First Name...'/><br/><br/>
+        <div className= "container">
+            <form>
+            <h3 ClassName='text-justify'>Sign Up to Create Soronko User Account</h3> 
+                <div className="row">
+                <div className="col-md-6">
+                    
+                    <label for= "fname">First Name</label><br/>
+                    <input type="text" id="fname" className="form-control" placeholder='Your First Name...'/><br/><br/>
 
-                <label for="lname">Last Name</label><br/>
-                <input type="text" id="lname" className="lname" placeholder='Your Last Name...'/><br/><br/>
+                    <label for="lname">Last Name</label><br/>
+                    <input type="text" id="lname" className="form-control" placeholder='Your Last Name...'/><br/><br/>
 
-                <label for="country">Country</label><br/>
-                <input type="text" id="country" className="country" placeholder="Ghana"/><br/><br/>
+                    <label for="country">Country</label><br/>
+                    <input type="text" id="country" className="form-control" placeholder="Ghana"/><br/><br/>
 
-                <label for="region">Region</label><br/>
-                <input type="text" id="region" className="region" placeholder="Volta Region"/><br/><br/>
+                    <label for="region">Region</label><br/>
+                    <input type="text" id="region" className="form-control" placeholder="Volta Region"/><br/><br/>
 
-                <label for="city"> City</label><br/>
-                <input type="text" id="city" className="city"/><br/><br/>
+                    <label for="city"> City</label><br/>
+                    <input type="text" id="city" className="form-control"/><br/><br/>
 
-                <label for="address">Street Address</label><br/>
-                <input type="text" id="address" className="address"/><br/><br/>
+                </div>
 
-                <label for="pnumber">Phone Number:</label><br/>
-                <input type="number" id="anumber" className="number" placeholder='+233(240000000)'/><br/><br/>
+                <div className="col-md-6">
+                    <label for="address">Street Address</label><br/>
+                    <input type="text" id="address" className="form-control"/><br/><br/>
 
-                <label for="email">Email</label><br/>
-                <input type="email" id="email" className="email" placeholder="@gmail.com"/><br/><br/>
+                    <label for="pnumber">Phone Number</label><br/>
+                    <input type="number" id="anumber" className="form-control" placeholder='+233(240000000)'/><br/><br/>
 
-                <label for="password">Password</label><br/>
-                <input type="password" id="password" className="password" placeholder="Must be at least 6 characters"/><br/><br/>
+                    <label for="email">Email</label><br/>
+                    <input type="email" id="email" className="form-control" placeholder="@gmail.com"/><br/><br/>
 
-                <label for="password">Confirm Password</label><br/>
-                <input type="password" id="password" className="password" placeholder="Must be at least 6 characters"/><br/><br/><br/>
+                    <label for="password">Password</label><br/>
+                    <input type="password" id="password" className="form-control" placeholder="Must be at least 6 characters"/><br/><br/>
 
-            <button onClick={handleclick}>Sign Up</button> 
+                    <label for="password">Confirm Password</label><br/>
+                    <input type="password" id="password" className="form-control" placeholder="Must be at least 6 characters"/><br/><br/><br/>
+                </div>
+
+                </div>
+                
+               
+
+            <button className="btn btn-primary" onClick={handleclick}>Sign Up</button> 
 
         </form>
+        </div>
     )
 }
 
